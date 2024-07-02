5b9ecf21c3b3ed36f9cbf751b70a27b06ab2571d
HARSHIT BANSAL <harshit.bansal1236@gmail.com>
2024-07-03 04:39:30 +0530

    Initial setup done

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+import lineClamp from '@tailwindcss/line-clamp'
+
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui, lineClamp],
+}
+
