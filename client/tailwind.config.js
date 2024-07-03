3ddff262a258f980565f51dc4495b5d3813691dc
HARSHIT BANSAL <harshit.bansal1236@gmail.com>
2024-07-03 22:59:50 +0530

    PostCSS added

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,11 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
-import lineClamp from '@tailwindcss/line-clamp'
 
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [daisyui, lineClamp],
+  plugins: [require('daisyui'), require('@tailwindcss/line-clamp')],
 }
 
